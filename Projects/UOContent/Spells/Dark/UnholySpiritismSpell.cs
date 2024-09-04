9d425e0bd97740dbf47c934d2c9cc4dbe7b3c560
yyoinkk <147203896+yyoinkk@users.noreply.github.com>
2024-09-04 14:28:26 +0300

    UnholySpiritism v1

--- Projects/UOContent/Spells/Dark/UnholySpiritismSpell.cs
+++ Projects/UOContent/Spells/Dark/UnholySpiritismSpell.cs
@@ -1,4 +1,8 @@
 
+using Server.Items;
+using System;
+using System.Collections.Generic;
+
 namespace Server.Spells.Dark
 {
     public class UnholySpiritismSpell : DarkSpell
@@ -17,11 +21,79 @@ namespace Server.Spells.Dark
         {
         }
 
+        private static readonly HashSet<Mobile> _table = new();
+
         public override SpellCircle Circle => SpellCircle.Fifth;
 
         public override void OnCast()
         {
-            Caster.LocalOverheadMessage(MessageType.Regular, 0x22, true, "Not Implemented yet...");
+            if (CheckSequence())
+            {
+                var damage = Math.Clamp(2 + (int)(Caster.Skills[CastSkill].Value / 10), 0, 16);
+                damage = damage > Caster.Hits ? Caster.Hits -1 : damage;
+
+                if (_table.Contains(Caster))
+                {
+                    damage = 0;
+                }
+
+                if (damage > 0)
+                {
+                    Caster.Damage(damage);
+                    Caster.SendMessage($"Damage received: {damage}");
+
+                    int mana = 14 + AOS.Scale(damage, 150);
+                    _table.Add(Caster);
+                    Timer.StartTimer(TimeSpan.FromSeconds(2.0), () => Delay_Callback(Caster, mana));
+                }
+            }
+        }
+
+        private void Delay_Callback(Mobile caster, int mana)
+        {
+            if (caster.Alive)
+            {
+                caster.Paralyzed = false;
+                caster.Frozen = false;
+                caster.Mana += mana;
+
+                caster.SendMessage($"Mana restored: {mana}");
+
+                caster.FixedParticles(0x3754, 10, 10, 0, EffectLayer.Head);
+                caster.PlaySound(0x1A2);
+            }
+
+            _table.Remove(caster);
+        }
+
+        public override bool CheckSequence()
+        {
+            if (Caster.Deleted || !Caster.Alive || Caster.Spell != this || State != SpellState.Sequencing)
+            {
+                DoFizzle();
+            }
+            else if (Scroll != null && (Scroll.Amount <= 0 || Scroll.Deleted || Scroll.RootParent != Caster))
+            {
+                DoFizzle();
+            }
+            else if (CheckFizzle())
+            {
+                if (Scroll is SpellScroll)
+                {
+                    Scroll.Consume();
+                }
+                else if (ClearHandsOnCast)
+                {
+                    Caster.ClearHands();
+                }
+                return true;
+            }
+            else
+            {
+                DoFizzle();
+            }
+
+            return false;
         }
     }
 }
