0e67e95144dbe05f04584d17685601db5c90ff08
yyoinkk <147203896+yyoinkk@users.noreply.github.com>
2024-09-04 19:55:48 +0300

    RegenerationSpell v1

--- Projects/UOContent/Spells/Dark/RegenerationSpell.cs
+++ Projects/UOContent/Spells/Dark/RegenerationSpell.cs
@@ -1,4 +1,6 @@
 using Server.Targeting;
+using System;
+using System.Collections.Generic;
 
 namespace Server.Spells.Dark
 {
@@ -19,6 +21,8 @@ namespace Server.Spells.Dark
 
         public override SpellCircle Circle => SpellCircle.Fifth;
 
+        private static readonly Dictionary<Mobile, InternalTimer> _table = new();
+
         public override void OnCast()
         {
             Caster.Target = new SpellTarget<Mobile>(this, TargetFlags.Beneficial);
@@ -26,7 +30,68 @@ namespace Server.Spells.Dark
 
         public void Target(Mobile m)
         {
-            Caster.LocalOverheadMessage(MessageType.Regular, 0x22, true, "Not Implemented yet...");
+            if (CheckBSequence(m))
+            {
+                SpellHelper.Turn(Caster, m);
+
+                if (!_table.TryGetValue(m, out var timer))
+                {
+                    int hp = Math.Clamp(1 + (int)(Caster.Skills[CastSkill].Value / 40), 1, 4);
+                    int duration = Math.Clamp(60 + (int)(GetDamageSkill(Caster) * 0.6), 60, 180);
+                    int interval = 4;
+
+                    timer = new InternalTimer(m, hp, TimeSpan.FromSeconds(duration), TimeSpan.FromSeconds(interval));
+                    _table[m] = timer;
+                    timer.Start();
+
+                    m.FixedParticles(0x374A, 10, 20, 0, EffectLayer.Waist);
+                    m.PlaySound(0x1E4);
+                }
+                else if (m == Caster)
+                {
+                    m.SendLocalizedMessage(502173); // You are already under a similar effect.
+                }
+                else
+                {
+                    m.SendLocalizedMessage(501775); // This spell is already in effect.
+                }
+            }
+        }
+
+        public static void ClearEffect(Mobile m)
+        {
+            _table.Remove(m, out var timer);
+            timer.Stop();
+
+            m.SendMessage("Regeneration is over.");
+        }
+
+        public static bool HasEffect(Mobile m) => _table.ContainsKey(m);
+
+        private class InternalTimer : Timer
+        {
+            private readonly Mobile _owner;
+            private readonly int _hp;
+            private readonly DateTime _endTime;
+
+            public InternalTimer(Mobile owner, int hp, TimeSpan duration, TimeSpan interval) : base(TimeSpan.FromSeconds(0.1), interval)
+            {
+                _owner = owner;
+                _hp = hp;
+                _endTime = Core.Now + duration;
+            }
+
+            protected override void OnTick()
+            {
+                if (!_owner.Alive || Core.Now >= _endTime)
+                {
+                    RegenerationSpell.ClearEffect(_owner);
+                    return;
+                }
+
+                _owner.Hits += _hp;
+                //_owner.SendMessage($"Healed: {_hp} hp.");
+            }
         }
     }
 }
