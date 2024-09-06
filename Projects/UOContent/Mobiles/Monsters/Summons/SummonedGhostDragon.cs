b6d5c25c1cb15a9a4e1dc40208589b8cb824c0e2
yyoinkk <147203896+yyoinkk@users.noreply.github.com>
2024-09-06 14:55:30 +0300

    GhostDragonSpell v1

---
+++ Projects/UOContent/Mobiles/Monsters/Summons/SummonedGhostDragon.cs
@@ -0,0 +1,61 @@
+using ModernUO.CodeGeneratedEvents;
+using ModernUO.Serialization;
+using Server.Items;
+using System.Collections.Generic;
+
+namespace Server.Mobiles
+{
+    [SerializationGenerator(0, false)]
+    public partial class SummonedGhostDragon : BaseCreature
+    {
+        [Constructible]
+        public SummonedGhostDragon() : base(AIType.AI_Mage)
+        {
+            Body = Utility.RandomList(60, 61);
+            BaseSoundID = 362;
+            Hue = 0x4001;
+
+            SetStr(201, 230);
+            SetDex(133, 152);
+            SetInt(101, 140);
+
+            SetHits(241, 258);
+
+            SetDamage(11, 17);
+
+            SetDamageType(ResistanceType.Physical, 50);
+            SetDamageType(ResistanceType.Cold, 30);
+            SetDamageType(ResistanceType.Energy, 20);
+
+            SetResistance(ResistanceType.Physical, 45, 50);
+            SetResistance(ResistanceType.Fire, 50, 60);
+            SetResistance(ResistanceType.Cold, 40, 50);
+            SetResistance(ResistanceType.Poison, 20, 30);
+            SetResistance(ResistanceType.Energy, 30, 40);
+
+            SetSkill(SkillName.MagicResist, 65.1, 80.0);
+            SetSkill(SkillName.Tactics, 65.1, 90.0);
+            SetSkill(SkillName.Wrestling, 65.1, 80.0);
+
+            VirtualArmor = 40;
+            ControlSlots = 4;
+
+            AddItem(new LightSource());
+        }
+
+        public override void OnDeath(Container c)
+        {
+            Effects.PlaySound(this, GetDeathSound());
+            base.OnDeath(c);
+        }
+
+        public override string DefaultName => "a ghost dragon";
+
+        public override bool DeleteCorpseOnDeath => true;
+
+        public override bool CanFly => true;
+
+        private static MonsterAbility[] _abilities = { MonsterAbilities.DrainLifeAttack };
+        public override MonsterAbility[] GetMonsterAbilities() => _abilities;
+    }
+}
--- Projects/UOContent/Spells/Dark/GhostDragonSpell.cs
+++ Projects/UOContent/Spells/Dark/GhostDragonSpell.cs
@@ -1,4 +1,9 @@
 
+using System;
+using Server.Mobiles;
+using System.Collections.Generic;
+using ModernUO.CodeGeneratedEvents;
+
 namespace Server.Spells.Dark
 {
     public class GhostDragonSpell : DarkSpell
@@ -20,9 +25,58 @@ namespace Server.Spells.Dark
 
         public override SpellCircle Circle => SpellCircle.Eighth;
 
+        public static readonly Dictionary<Mobile, SummonedGhostDragon> _table = new();
+
         public override void OnCast()
         {
-            Caster.LocalOverheadMessage(MessageType.Regular, 0x22, true, "Not Implemented yet...");
+            if (CheckSequence())
+            {
+                var duration = Core.Expansion switch
+                {
+                    Expansion.None => TimeSpan.FromSeconds(Caster.Skills.Magery.Value),
+                    // T2A -> Current
+                    _ => TimeSpan.FromSeconds(3 * Math.Max(5, Caster.Skills.Magery.Value)),
+                };
+
+                Caster.SendMessage($"Duration: {duration:mm\\:ss}s.");
+
+                SummonedGhostDragon dragon = new SummonedGhostDragon();
+                SpellHelper.Summon(dragon, Caster, 0x216, duration, false, false);
+
+                if (!dragon.Deleted)
+                {
+                    dragon.FixedParticles(0x3728, 8, 20, 5042, EffectLayer.Head);
+                    _table[Caster] = dragon;
+                }
+            }
+
+            FinishSequence();
+        }
+
+        [OnEvent(nameof(PlayerMobile.PlayerDeathEvent))]
+        public static void OnPlayerDeathEvent(Mobile m)
+        {
+            if (_table.TryGetValue(m, out var dragon))
+            {
+                dragon.Kill();
+                _table.Remove(m);
+            }
+        }
+
+        public override bool CheckCast()
+        {
+            if (!base.CheckCast())
+            {
+                return false;
+            }
+
+            if (Caster.Followers + 4 > Caster.FollowersMax)
+            {
+                Caster.SendLocalizedMessage(1049645); // You have too many followers to summon that creature.
+                return false;
+            }
+
+            return true;
         }
     }
 }
