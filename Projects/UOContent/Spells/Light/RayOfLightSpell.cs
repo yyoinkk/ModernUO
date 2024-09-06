d625332f3486db99007267e0f11d359fe6a75182
yyoinkk <147203896+yyoinkk@users.noreply.github.com>
2024-09-06 23:17:01 +0300

    RayOfLightSpell v1

--- Projects/UOContent/Spells/Light/RayOfLightSpell.cs
+++ Projects/UOContent/Spells/Light/RayOfLightSpell.cs
@@ -26,7 +26,20 @@ namespace Server.Spells.Light
 
         public void Target(Mobile m)
         {
-            Caster.LocalOverheadMessage(MessageType.Regular, 0x22, true, "Not Implemented yet...");
+            if (CheckHSequence(m))
+            {
+                SpellHelper.Turn(Caster, m);
+
+                SpellHelper.CheckReflect((int)Circle, Caster, ref m);
+
+                double damage = GetNewAosDamage(17, 1, 9, m);
+
+                m.FixedEffect(0x37B9, 14, 22, 2733, 0);
+                m.FixedEffect(0x374A, 14, 14, 2733, 0);
+                m.PlaySound(0x1E9);
+
+                SpellHelper.Damage(this, m, damage, 0, 50, 0, 0, 50);
+            }
         }
     }
 }
