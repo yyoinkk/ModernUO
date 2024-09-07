e71f330157b31bd2c5454401c1e36bff03527e75
yyoinkk <147203896+yyoinkk@users.noreply.github.com>
2024-09-07 17:35:34 +0300

    DivineSpell v1

--- Projects/UOContent/Spells/Light/DivineInterventionSpell.cs
+++ Projects/UOContent/Spells/Light/DivineInterventionSpell.cs
@@ -1,4 +1,7 @@
+using System;
 using Server.Targeting;
+using Server.Spells.Dark;
+using System.Collections.Generic;
 
 namespace Server.Spells.Light
 {
@@ -21,6 +24,22 @@ namespace Server.Spells.Light
 
         public override SpellCircle Circle => SpellCircle.Eighth;
 
+        private static readonly TimeSpan CD = TimeSpan.FromSeconds(180);
+
+        private static readonly HashSet<Mobile> _table = new();
+
+        public override bool CheckCast()
+        {
+            if (_table.Contains(Caster))
+            {
+                Caster.SendMessage("You can't use Divine yet.");
+                return false;
+            }
+            return true;
+        }
+
+        public override void OnCasterHurt() { }
+
         public override void OnCast()
         {
             Caster.Target = new SpellTarget<Mobile>(this, TargetFlags.Beneficial);
@@ -28,7 +47,35 @@ namespace Server.Spells.Light
 
         public void Target(Mobile m)
         {
-            Caster.LocalOverheadMessage(MessageType.Regular, 0x22, true, "Not Implemented yet...");
+            if (CheckBSequence(m))
+            {
+                SpellHelper.Turn(Caster, m);
+
+                m.Hits = m.HitsMax;
+                m.Mana = m.ManaMax;
+                m.Stam = m.StamMax;
+
+                VulnerabilitySpell.ClearEffect(m);
+                LowerResistanceSpell.ClearEffect(m);
+                DarknessSpell.ClearEffect(m);
+
+                m.PlaySound(0x214);
+                m.PlaySound(0x1FD);
+
+                m.FixedEffect(0x375A, 20, 15, 2066, 0);
+                m.FixedEffect(0x373A, 10, 15, 2066, 0);
+
+                _table.Add(Caster);
+                Timer.StartTimer(CD, () => EndCD(Caster));
+            }
+        }
+
+        public static void EndCD(Mobile m)
+        {
+            if (_table.Remove(m))
+            {
+                m.SendMessage("You can use Divine again.");
+            }
         }
     }
 }
