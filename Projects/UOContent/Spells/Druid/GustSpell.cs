832095835055501a20f24cb7781ab6a9d2e83df9
yyoinkk <147203896+yyoinkk@users.noreply.github.com>
2024-09-10 12:08:57 +0300

    GUstSpell v1

--- Projects/UOContent/Spells/Druid/GustSpell.cs
+++ Projects/UOContent/Spells/Druid/GustSpell.cs
@@ -1,3 +1,4 @@
+using Server.Regions;
 using Server.Targeting;
 
 namespace Server.Spells.Druid
@@ -26,7 +27,71 @@ namespace Server.Spells.Druid
 
         public void Target(Mobile m)
         {
-            Caster.LocalOverheadMessage(MessageType.Regular, 0x22, true, "Not Implemented yet...");
+            if (CheckHSequence(m))
+            {
+                var dir = Caster.GetDirectionTo(m);
+                var oppositeDir = m.GetDirectionTo(Caster);
+                SpellHelper.Turn(Caster, m);
+
+                SpellHelper.CheckReflect((int)Circle, Caster, ref m);
+
+                double damage = GetNewAosDamage(23, 1, 4, m);
+
+                m.FixedParticles(0x37CC, 10, 30, 5052, EffectLayer.LeftFoot);
+                m.PlaySound(0x212);
+
+                var direction = Caster == m ? oppositeDir : dir;
+                m.Location = GetNewPoint(m, direction);
+
+                SpellHelper.Damage(this, m, damage, 0, 50, 0, 0, 50);
+            }
+        }
+
+        private Point3D GetNewPoint(Mobile m, Direction dir)
+        {
+            int range = 3;
+
+            var oldLoc = m.Location;
+            var newLoc = m.Location;
+
+            for (int i = 1; i <= range; i++)
+            {
+                var newValidLoc = oldLoc;
+                Movement.Movement.Offset(dir, ref newValidLoc, i);
+                if (IsValidLocation(newValidLoc, m))
+                {
+                    newLoc = newValidLoc;
+                }
+                else
+                {
+                    return newLoc;
+                }
+            }
+
+            return newLoc;
+        }
+
+        private bool IsValidLocation(Point3D loc, Mobile m)
+        {
+            Map map = m.Map;
+            if (map == null)
+            {
+                return false;
+            }
+            else if (!map.CanSpawnMobile(loc.X, loc.Y, loc.Z, checkMobiles: false))
+            {
+                return false;
+            }
+            else if (SpellHelper.CheckMulti(loc, map))
+            {
+                return false;
+            }
+            else if (Region.Find(loc, map).IsPartOf<HouseRegion>())
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
